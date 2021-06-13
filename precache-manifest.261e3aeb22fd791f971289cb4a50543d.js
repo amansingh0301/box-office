4666e5d925b23a2137d726c1f76ab723cfde34ec
amansingh0301 <as9997450@gmail.com>
2021-06-13 10:50:22 +0530

    Updates

---
+++ precache-manifest.261e3aeb22fd791f971289cb4a50543d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d531fd60b604f270ea0ef3500658645",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "79c88b99d9216b5cff91",
+    "url": "/box-office/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f1e37c3990449b2e327e",
+    "url": "/box-office/static/js/2.c08e71e6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office/static/js/2.c08e71e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79c88b99d9216b5cff91",
+    "url": "/box-office/static/js/main.dfbd0f2b.chunk.js"
+  },
+  {
+    "revision": "c850b6c555a3d841251e",
+    "url": "/box-office/static/js/runtime-main.e4c5afaf.js"
+  }
+]);
